3c4eab21f4f4e2a201e64d8c5d3f68b07a5fecfe
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-31 20:28:03 +0530

    show records in view as table

--- website/views.py
+++ website/views.py
@@ -3,9 +3,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
+
     # check to see if logging in
     if request.method == "POST":
         username = request.POST["username"]
@@ -26,7 +29,7 @@ def home(request):
             return redirect("home")
 
     else:
-        return render(request, "home.html", {})
+        return render(request, "home.html", {"records": records})
 
 
 def logout_user(request):
