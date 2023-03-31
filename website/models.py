fbe4b2490a89a016e2f62387b58475eb6c8af33b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-31 20:08:14 +0530

    created model and migrated to db

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.CharField(max_length=40)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=40)
+    state = models.CharField(max_length=40)
+    zipcode = models.CharField(max_length=40)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
