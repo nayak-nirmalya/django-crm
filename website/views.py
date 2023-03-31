5174952d69c3ac929bafe0de67fab5d066faddd0
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-31 21:11:09 +0530

    added form and view for adding a record

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -84,4 +84,15 @@ def delete_record(request, pk):
 
 
 def add_record(request):
-    return render(request, "add_record.html", {})
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added.")
+                return redirect("home")
+
+        return render(request, "add_record.html", {"form": form})
+    else:
+        messages.success(request, "Not Authenticated!")
+        return redirect("home")
