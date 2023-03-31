b5d55a9f03dbb68d5c9c07e612d23c1686a54664
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-31 19:03:33 +0530

    added logic for logout

--- website/views.py
+++ website/views.py
@@ -27,9 +27,7 @@ def home(request):
         return render(request, "home.html", {})
 
 
-# def login_user(request):
-#     pass
-
-
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You Have Been Logged Out.")
+    return redirect("home")
